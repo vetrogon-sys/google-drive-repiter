e9a26f9c53e95c6be4c937a5b4add1c581335618
Kostya <kostyabogdanov338@gmail.com>
2024-04-07 18:10:42 +0300

    initial commit

---
+++ bot-src/src/main/java/com/google/driverepeater/botsrc/BotSrcApplication.java
@@ -0,0 +1,13 @@
+package com.google.driverepeater.botsrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotSrcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotSrcApplication.class, args);
+    }
+
+}
---
+++ bot-src/src/test/java/com/google/driverepeater/botsrc/BotSrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.driverepeater.botsrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotSrcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
